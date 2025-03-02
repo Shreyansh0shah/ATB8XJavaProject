0adfa3e4bd42ee7cf34cb2c5aa381cb5a0257b2e
shahshreyansh <sshreyansh0000@gmail.com>
2025-03-02 23:42:04 +0530

    Add This Files

---
+++ src/sept/exe_14092024/Lab037.java
@@ -0,0 +1,15 @@
+package sept.exe_14092024;
+
+public class Lab037 {
+    public static void main(String[] args) {
+        char a1 = 'A'; // 65
+        System.out.println(a1);
+        char b1 = 'b'; // 66
+        int c1 = a1 + b1;
+        System.out.println(c1); //131
+        short s = 10;
+        char c = 'A';
+        int result = s+c;
+        System.out.println(result);
+    }
+}
---
+++ src/sept/exe_14092024/Lab038.java
@@ -0,0 +1,9 @@
+package sept.exe_14092024;
+
+public class Lab038 {
+    public static void main(String[] args) {
+        //type casting
+
+    }
+
+}
